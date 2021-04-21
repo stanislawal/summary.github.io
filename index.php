96c2d91858a3cddb8625023f88af4715f778b4e1
stanislawal <66276514+stanislawal@users.noreply.github.com>
2021-04-21 12:22:50 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
+            crossorigin="anonymous"></script>
+    <title>Test serve</title>
+</head>
+<?
+include('layouts/header.php');
+?>
+<body class="bg-dark">
+<div class="container text-center">
+    <div>
+        <h1 class="mt-5 text-white">Составь своё резюме</h1>
+    </div>
+    <div>
+
+        </form>
+    </div>
+</div>
+
+</body>
+
+</html>
---
+++ layouts/header.php
@@ -0,0 +1,44 @@
+<div class="bg-dark collapse" id="navbarHeader" style="">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 col-md-7 py-4">
+                <h4 class="text-white">О нас</h4>
+                <p class="text-muted">Вы можете составить своё резюме для нашей компании. Скролл вниз определит ваше
+                    будущее</p>
+            </div>
+            <div class="col-sm-4 offset-md-1 py-4">
+                <h4 class="text-white">Контакты</h4>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white">Подписать на Twitter</a></li>
+                    <li><a href="#" class="text-white">Лайк на Facebook</a></li>
+                    <li><a href="#" class="text-white">Напишите мне/a&gt;</a></li>
+                    <a href="#" class="text-white">
+                    </a></ul>
+                <a href="#" class="text-white">
+                </a></div>
+            <a href="#" class="text-white">
+            </a></div>
+        <a href="#" class="text-white">
+        </a></div>
+    <a href="#" class="text-white">
+    </a></div><a href="#" class="text-white">
+</a>
+<div class="navbar navbar-dark bg-dark shadow-sm"><a href="#" class="text-white">
+    </a>
+    <div class="container"><a href="#" class="text-white">
+        </a><a href="#" class="navbar-brand d-flex align-items-center">
+            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bookmark-check" fill="currentColor"
+                 xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                      d="M4.5 2a.5.5 0 0 0-.5.5v11.066l4-2.667 4 2.667V8.5a.5.5 0 0 1 1 0v6.934l-5-3.333-5 3.333V2.5A1.5 1.5 0 0 1 4.5 1h4a.5.5 0 0 1 0 1h-4z"/>
+                <path fill-rule="evenodd"
+                      d="M15.854 2.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 4.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+            </svg>
+            <strong>Резюме</strong>
+        </a>
+        <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader"
+                aria-controls="navbarHeader" aria-expanded="false" aria-label="Переключить навигацию">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+</div>
